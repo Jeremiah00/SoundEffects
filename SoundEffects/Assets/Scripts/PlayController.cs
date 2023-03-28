a63545d127171d37ed8e8b6f68d542ab980d55a6
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2023-03-28 11:43:02 -0700

    p
    p

--- SoundEffects/Assets/Scripts/PlayController.cs
+++ SoundEffects/Assets/Scripts/PlayController.cs
@@ -6,18 +6,29 @@ public class PlayController : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 1000);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
